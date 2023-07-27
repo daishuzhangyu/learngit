c440eb147c37f6b46a15fb0401bd47ef889bfe01
v3edu <525208924@qq.com>
2023-07-27 23:24:31 +0800

    use always

--- learngit/net7_verilogbase/and_logic_prj/hdl/and_logic.v
+++ learngit/net7_verilogbase/and_logic_prj/hdl/and_logic.v
@@ -14,9 +14,12 @@ module and_logic
 	  input wire    key1, 
       input  wire   key2, 
 
-      output wire   led 
+      output reg   led 
 ); 
 
-assign led = key1&key2; 
+//assign led = key1&key2; 
+
+always@(*)
+	led = key1&key2; 
 
 endmodule
